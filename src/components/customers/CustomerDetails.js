06bd204b453446534cb260e8301fe6a1decd7563
Jeremy White <jcwhite414@gmail.com>
2022-11-01 17:44:16 -0500

    finished update loyalty number form

--- src/components/customers/CustomerDetails.js
+++ src/components/customers/CustomerDetails.js
@@ -17,13 +17,53 @@ export const CustomerDetails = () => {
     fetchData();
   }, []);
 
+  const handleSubmit = (click) => {
+    click.preventDefault();
+    const postData = async (data) => {
+      //declare fetchOptions
+      const fetchOptions = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      };
+      //fetch stringified entry obj
+      const response = await fetch(
+        `http://localhost:8088/customers/${customerId}`,
+        fetchOptions
+      );
+      //handle response
+    };
+    postData(customer);
+  };
+
   return (
     <>
       <h3>Name: {customer.user?.fullName}</h3>
       <p>Address: {customer?.address}</p>
       <p>Phone Number: {customer?.phoneNumber}</p>
-      <p>Loyalty #: {customer?.loyalty}</p>
       <p>Email: {customer.user?.email}</p>
+      <form>
+        <fieldset>
+          <label htmlFor="loyaltyNumber">Loyalty Number</label>
+          <input
+            required
+            autoFocus
+            type="number"
+            className="form-control"
+            value={customer.loyalty}
+            onChange={(evt) => {
+              const formCopy = { ...customer };
+              formCopy.loyalty = evt.target.value;
+              setCustomer(formCopy);
+            }}
+          />
+        </fieldset>
+        <button onClick={(click) => handleSubmit(click)}>
+          Update Loyalty Number
+        </button>
+      </form>
     </>
   );
 };
